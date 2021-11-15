9f2ea76a03b52218e091e23a3e46cad39935df76
Jaden Andrews <59934265+jadenandrews831@users.noreply.github.com>
2021-11-15 07:41:29 -0500

    Commit

---
+++ this & Object Prototypes/Objects/Part III/implicitMixins.js
@@ -0,0 +1,22 @@
+var Something = {
+  cool: function()
+  {
+    this.greeting = "Hello World!",
+    this.count = this.count ? this.count++ : this.count = 1 
+  }
+}
+
+Something.cool();
+console.log(Something.greeting);
+console.log(Something.count);
+
+var Another = {
+  cool: function()
+  {
+    Something.cool.call( this );
+  }
+}
+
+Another.cool();
+console.log(Another.greeting);
+console.log(Another.count);
---
+++ this & Object Prototypes/Objects/Part III/parasiticInheritance.js
@@ -0,0 +1,42 @@
+/* eslint-disable no-unused-vars */
+function Vehicle()
+{
+  this.engines = 1;
+}
+Vehicle.prototype.ignition = function()
+{
+  console.log("Turning on my engine.");
+};
+Vehicle.prototype.drive = function()
+{
+  this.ignition();
+  console.log( "Steering and moving forward!" );
+}
+
+// "Parasitic Class" 'Car'
+function Car()
+{
+  // first, 'car' is a 'Vehicle'
+  var car = new Vehicle();
+
+  // now, let's modify our 'car' to specialize it
+  car.wheels = 4;
+
+  // save a privileged reference to 'Vehicle::drive()'
+  var vehDrive = car.drive;
+
+  // override 'Vehicle::drive()'
+  car.drive = function()
+  {
+    vehDrive.call(this);
+    console.log(
+      "Rolling on all ", this.wheels, "wheels!"
+    );
+  }
+  return car;
+}
+
+var myCar = Car();
+
+myCar.drive();
+console.log(myCar);
